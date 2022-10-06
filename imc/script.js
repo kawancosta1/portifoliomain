4d3e1e062863d95f5532354478077cebde13bc5e
kawan <98924310+kawancosta1@users.noreply.github.com>
2022-10-06 16:57:27 -0300

    imc

---
+++ imc/script.js
@@ -0,0 +1,41 @@
+
+var calcular = document.getElementById("calcular");
+
+calcular.addEventListener("click", imc)
+function imc(){
+    var result = document.getElementById("result")
+    var inome = document.getElementById("Inome").value;
+var ipeso = document.getElementById("Ipeso").value;
+var ialtura = document.getElementById("Ialtura").value;
+
+
+if(inome == "" || ipeso == ""||ialtura == ""){
+   
+}else{
+
+    // tofixade = arredondar 1 casas
+    var alt = ialtura * ialtura
+    var imc = ( ipeso / alt).toFixed(1)
+    if(imc < 18.5){
+        result.textContent = `${inome} seu imc é ${imc} ${classifica}`;
+        result.textContent = `${inome} seu imc é ${imc} ${classifica}`;
+    }else if(imc > 18.5 && imc< 24.9){
+        var classifica = "você está no peso ideal SEU LIXO"
+        result.textContent = `${inome} seu imc é ${imc} ${classifica}`;
+    }else if(imc > 25 && imc < 29){
+        classifica = "você está acima do peso"
+        result.textContent = `${inome} seu imc é ${imc} ${classifica}`;
+    }else if(imc > 30 && imc < 34.9){
+        classifica = "você está obeso"
+        result.textContent = `${inome} seu imc é ${imc} ${classifica}`;
+    }else if(imc > 35 ){
+        classifica = "obesidade morbita"
+result.textContent = `${inome} seu imc é ${imc} ${classifica}`;
+    }
+   
+    
+
+
+}
+
+}
