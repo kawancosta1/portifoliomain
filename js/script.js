b71de73f54510be45dcb175721c3096f5cab1f1c
kawan <kawancosta063@gmail.com>
2023-01-15 12:17:17 -0300

    adicionando animações

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 
 
 
-
-
+// imc
 function passar(){
+    
     let imagem = document.querySelector('.Naparece')
     imagem.classList.toggle('aparece')
    
@@ -13,4 +13,18 @@ function passar(){
 function retira(){
     let imagem = document.querySelector('.Naparece')
     imagem.classList.toggle('aparece')
+}
+
+//Star
+function passarStar(){
+    
+    let imagem2 = document.querySelector('.NapareceStar')
+    imagem2.classList.toggle('apareceStar')
+   
+
+}
+
+function retirarStar(){
+    let imagem2 = document.querySelector('.NapareceStar')
+    imagem2.classList.toggle('apareceStar')
 }
